82987d46b7c2a7194fe2e0dfd2e0cf7a187d25d9
Julean Hommel <julean.hommel204879@student.leeuwenborgh.nl>
2017-11-28 13:45:08 +0100

    something?

--- 02. Controls/MainWindow.xaml.cs
+++ 02. Controls/MainWindow.xaml.cs
@@ -240,19 +240,19 @@ namespace _02.Controls
         {
             if(btnButton1.Content == "X" && btnButton2.Content == "X" && btnButton3.Content == "X" || btnButton4.Content == "X" && btnButton5.Content == "X" && btnButton6.Content == "X" || btnButton7.Content == "X" && btnButton8.Content == "X" && btnButton9.Content == "X" || btnButton1.Content == "X" && btnButton4.Content == "X" && btnButton7.Content == "X" || btnButton2.Content == "X" && btnButton5.Content == "X" && btnButton8.Content == "X" || btnButton3.Content == "X" && btnButton6.Content == "X" && btnButton9.Content == "X" || btnButton1.Content == "X" && btnButton5.Content == "X" && btnButton9.Content == "X" || btnButton3.Content == "X" && btnButton5.Content == "X" && btnButton7.Content == "X")
             {
-                MessageBox.Show("X heeft gewonnen!", "gefeliciteerd");
+                MessageBox.Show("X has won!", "Congratulations");
                 Button_Click(null, null);
                 Xwins++;
             }
             else if (btnButton1.Content == "O" && btnButton2.Content == "O" && btnButton3.Content == "O" || btnButton4.Content == "O" && btnButton5.Content == "O" && btnButton6.Content == "O" || btnButton7.Content == "O" && btnButton8.Content == "O" && btnButton9.Content == "O" || btnButton1.Content == "O" && btnButton4.Content == "O" && btnButton7.Content == "O" || btnButton2.Content == "O" && btnButton5.Content == "O" && btnButton8.Content == "O" || btnButton3.Content == "O" && btnButton6.Content == "O" && btnButton9.Content == "O" || btnButton1.Content == "O" && btnButton5.Content == "O" && btnButton9.Content == "O" || btnButton3.Content == "O" && btnButton5.Content == "O" && btnButton7.Content == "O")
             {
-                MessageBox.Show("O heeft gewonnen!", "gefeliciteerd");
+                MessageBox.Show("O has won!", "Congratulations");
                 Button_Click(null, null);
                 Owins++;
             }
             else if(btnButton1.Content != "" && btnButton2.Content != "" && btnButton3.Content != "" && btnButton4.Content != "" && btnButton5.Content != "" && btnButton6.Content != "" && btnButton7.Content != "" && btnButton8.Content != "" && btnButton9.Content != "")
             {
-                MessageBox.Show("Het is geen gelijk spel!!!", "");
+                MessageBox.Show("It's a tie!!!", "");
                 Button_Click(null, null);
             }
         }
